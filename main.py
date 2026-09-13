bcdff0726d842856b1c457afd55215af5d328920
pearl-python <214028959+pearl-python@users.noreply.github.com>
2026-09-13 22:13:07 +0900

    Add file error handling and reusable log filtering

--- main.py
+++ main.py
@@ -1,36 +1,39 @@
 #log読み込み関数
 def load_logs(file_path):
     logs = []
-    with open(file_path,"r",encoding="utf-8") as file:
-        for log in file:
-            logs.append(log.strip())
-    return logs
+    try:
+        with open(file_path,"r",encoding="utf-8") as file:
+            for log in file:
+                logs.append(log.strip())
+    except FileNotFoundError as e:
+        print(f"ファイルがありません{e}")
+        return None
+    else:
+        return logs
 
 #エラー行特定関数
-def get_error_logs(logs):
-    error_logs = []
-    for log in logs:
-        if "ERROR" in log:
-            error_logs.append(log)
-    return error_logs
+def get_level_logs(logs, level):
+    matched_logs = []
 
-def get_warning_logs(logs):
-    warning_logs = []
     for log in logs:
-        if "WARNING" in log:
-            warning_logs.append(log)
-    return warning_logs
+        if level in log:
+            matched_logs.append(log)
+
+    return matched_logs
 
 #logファイル出力
 logs = load_logs("sample/app.log")
-error_logs = get_error_logs(logs)
-warning_logs = get_warning_logs(logs)
+if logs is None:
+    print("エラーのため終了します")
+else:
+    error_logs = get_level_logs(logs,"ERROR")
+    warning_logs = get_level_logs(logs,"WARNING")
 
-for log in error_logs:
-    print(log)
+    for log in error_logs:
+        print(log)
 
-for log in warning_logs:
-    print(log)
+    for log in warning_logs:
+        print(log)
 
-print(f"ERROR件数: {len(error_logs)}")
-print(f"WARNING件数: {len(warning_logs)}")
+    print(f"ERROR件数: {len(error_logs)}")
+    print(f"WARNING件数: {len(warning_logs)}")
