a3cc51b125ba2432a872a02164b41fee1f160f19
pearl-python <214028959+pearl-python@users.noreply.github.com>
2026-09-13 23:41:51 +0900

    Add automatic log level detection

--- main.py
+++ main.py
@@ -1,39 +1,46 @@
 #log読み込み関数
 def load_logs(file_path):
     logs = []
+    levels = []
     try:
         with open(file_path,"r",encoding="utf-8") as file:
             for log in file:
                 logs.append(log.strip())
+                levels.append(get_level(log))
     except FileNotFoundError as e:
         print(f"ファイルがありません{e}")
-        return None
+        return None, None
     else:
-        return logs
+        unique_level = []
+        for level in levels:
+            if level not in unique_level:
+                unique_level.append(level)
 
-#エラー行特定関数
+        return logs,unique_level
+
+# ログレベル抽出関数
 def get_level_logs(logs, level):
     matched_logs = []
 
     for log in logs:
-        if level in log:
+        if get_level(log) == level:
             matched_logs.append(log)
 
     return matched_logs
 
+def get_level(log):
+    parts = log.split()
+    level = parts[2]
+    return level
+
 #logファイル出力
-logs = load_logs("sample/app.log")
+logs,unique_level = load_logs("sample/app.log")
+
 if logs is None:
     print("エラーのため終了します")
 else:
-    error_logs = get_level_logs(logs,"ERROR")
-    warning_logs = get_level_logs(logs,"WARNING")
-
-    for log in error_logs:
-        print(log)
-
-    for log in warning_logs:
-        print(log)
-
-    print(f"ERROR件数: {len(error_logs)}")
-    print(f"WARNING件数: {len(warning_logs)}")
+    for level in unique_level:
+        level_logs = get_level_logs(logs,level)
+        print(f"{level}件数: {len(level_logs)}")
+        for log in level_logs:
+            print(log)        
