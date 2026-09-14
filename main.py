5a4b165a4c8d82d7d6d4da88854e17bcc274433b
pearl-python <214028959+pearl-python@users.noreply.github.com>
2026-09-14 18:45:53 +0900

    Handle invalid log lines

--- main.py
+++ main.py
@@ -2,21 +2,28 @@
 def load_logs(file_path):
     logs = []
     levels = []
+    invalid_logs = []
     try:
         with open(file_path,"r",encoding="utf-8") as file:
             for log in file:
-                logs.append(log.strip())
-                levels.append(get_level(log))
+                log = log.strip()
+                level = get_level(log)
+
+                if level is None:
+                    invalid_logs.append(log)
+                    continue
+                logs.append(log)
+                levels.append(level)
     except FileNotFoundError as e:
         print(f"ファイルがありません{e}")
-        return None, None
+        return None, None, None
     else:
         unique_level = []
         for level in levels:
             if level not in unique_level:
                 unique_level.append(level)
 
-        return logs,unique_level
+        return logs,unique_level,invalid_logs
 
 # ログレベル抽出関数
 def get_level_logs(logs, level):
@@ -28,13 +35,16 @@ def get_level_logs(logs, level):
 
     return matched_logs
 
+#エラーレベル取得関数
 def get_level(log):
     parts = log.split()
-    level = parts[2]
-    return level
+    if len(parts) < 3:
+        return None
+    else:    
+        return parts[2] 
 
 #logファイル出力
-logs,unique_level = load_logs("sample/app.log")
+logs,unique_level,invalid_logs = load_logs("sample/app.log")
 
 if logs is None:
     print("エラーのため終了します")
@@ -43,4 +53,7 @@ else:
         level_logs = get_level_logs(logs,level)
         print(f"{level}件数: {len(level_logs)}")
         for log in level_logs:
-            print(log)        
+            print(log)
+    print(f"不正ログ件数：{len(invalid_logs)}")
+    for log in invalid_logs:
+        print(log)
